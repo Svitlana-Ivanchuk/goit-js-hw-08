b6ecab563d231f64dbd2ad8059cb280ed428e57c
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-07-17 13:51:18 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,41 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from '/node_modules/simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 // Change code below this line
 
 console.log(galleryItems);
-
-const galleryCollection = document.querySelector('.gallery');
-const cardsMarkup = createImageCardMarkup(galleryItems);
-
-galleryCollection.insertAdjacentHTML('beforeend', cardsMarkup);
-// ця функція при роботі з бібліотекою не потрібна
-// galleryCollection.addEventListener('click', handelGalleryCollectionClick);
-
-galleryCollection.style.listStyle = 'none';
-
-function createImageCardMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-    <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-    </li>`;
-    })
-    .join('');
-}
-
-// function handelGalleryCollectionClick(evt) {
-//   evt.preventDefault();
-//   if (evt.target.nodeName !== 'IMG') return;
-
-let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});
-// }
